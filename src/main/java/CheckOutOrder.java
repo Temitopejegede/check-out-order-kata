6f7fd217da54d5ee298c7c69e1621afa0a52ae37
temitopejegede <temiteejegede@gmail.com>
2022-07-20 00:59:10 -0400

    renamed the class to something more intuitive

---
+++ src/main/java/CheckOutOrder.java
@@ -0,0 +1,26 @@
+import java.math.BigDecimal;
+
+public class CheckOutOrder {
+    public CheckOutOrder() {
+    }
+
+    void addToOrder(String itemName){
+
+    }
+
+    private void addToOrder(String itemName, double itemWeight){
+
+    }
+
+    void removeFromOrder(String itemName){
+
+    }
+
+    public BigDecimal getTotalAmount() {
+
+        return null;
+    }
+
+    //need methods for markdown
+    //need methods for when there is a promo like by 1 get 1 free
+}
