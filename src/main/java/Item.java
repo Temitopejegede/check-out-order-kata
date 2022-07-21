1907cb2f8db737898c203f3d142c62f5c406a6b7
temitopejegede <temiteejegede@gmail.com>
2022-07-21 17:12:28 -0400

    added a price field for enums

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,3 +1,18 @@
+import java.math.BigDecimal;
+
 public enum Item {
-    BANANA, CARROT, PINEAPPLE, NOODLES, STRAWBERRY
+    BANANA(BigDecimal.valueOf(0.10)),
+    CARROT(BigDecimal.valueOf(0.5)),
+    PINEAPPLE(BigDecimal.valueOf(1.5)),
+    NOODLES(BigDecimal.valueOf(0.23)),
+    STRAWBERRY(BigDecimal.valueOf(0.05));
+
+    public final BigDecimal price;
+    Item(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getPrice(){
+        return price;
+    }
 }
