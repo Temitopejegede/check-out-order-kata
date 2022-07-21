96b046cb7673aa44fc4839dc83c101942f620cf3
temitopejegede <temiteejegede@gmail.com>
2022-07-21 14:22:48 -0400

    added more tests

--- src/test/java/ThrowsErrorTests.java
+++ src/test/java/ThrowsErrorTests.java
@@ -1,20 +1,26 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ThrowsErrorTests {
 
     @Test
-    void itemNotFoundTest(){
+    void itemNotFoundTest1(){
         assertThrows(IllegalArgumentException.class,
-                new Executable() {
-                    @Override
-                    public void execute() throws Throwable {
+                () -> {
                         CheckOutOrder checkOutOrder = new CheckOutOrder();
                         checkOutOrder.addToOrder("avocado");
-                    }
                 });
+    }
+
+    @Test
+    void itemNotFoundTest2(){
+        assertDoesNotThrow(() -> {
+            CheckOutOrder checkOutOrder = new CheckOutOrder();
+            checkOutOrder.addToOrder("Banana");
+        });
     }
 
 
