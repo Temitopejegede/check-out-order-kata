af5b23445a52e59f78253c6acbc04fe2c7196f81
temitopejegede <temiteejegede@gmail.com>
2022-07-21 17:11:49 -0400

    added price list

--- src/main/java/CheckOutOrder.java
+++ src/main/java/CheckOutOrder.java
@@ -1,11 +1,17 @@
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CheckOutOrder {
 
     List<String> itemList = new ArrayList<>();
     List<String> activeDeals = new ArrayList<>();
+
+    Map<String, BigDecimal> prices = new HashMap<>();
+
+    public BigDecimal total = BigDecimal.valueOf(0);
     public CheckOutOrder() {
 
     }
@@ -14,7 +20,8 @@ public class CheckOutOrder {
         this.activeDeals = activeDeals;
     }
 
-    void addToOrder(String itemName) throws IllegalArgumentException{
+    void addToOrder(String itemName)
+            throws IllegalArgumentException{
         for(Item c : Item.values()){
             if(c.name().equalsIgnoreCase(itemName)){
                 String item = c.name();
@@ -38,8 +45,13 @@ public class CheckOutOrder {
     }
 
     public BigDecimal getTotalAmount() {
-
         return null;
+    }
+
+    public void makePriceList(){
+        for(Item c : Item.values()){
+            prices.put(c.name(), c.getPrice());
+        }
     }
 
 
