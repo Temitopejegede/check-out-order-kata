5c1810c8eb4bbc7bf5c0d09807dd55ffa8250055
temitopejegede <temiteejegede@gmail.com>
2022-07-24 02:30:48 -0400

    new testcases

--- src/test/java/ItemTotalTests.java
+++ src/test/java/ItemTotalTests.java
@@ -3,39 +3,32 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ItemTotalTests {
 
-    @Test
-    void scannedItemsTotalTest1(){
-        CheckOutOrder checkOutOrder = new CheckOutOrder();
-        checkOutOrder.addToOrder("banana");
-        checkOutOrder.addToOrder("mango");
-        checkOutOrder.addToOrder("tyson drumstick");
-        BigDecimal totalOfItems = checkOutOrder.getTotalAmount();
-        assertEquals(BigDecimal.valueOf(10), totalOfItems);
-    }
+
 
     @Test
     void scannedItemsTotalTest2(){
         CheckOutOrder checkOutOrder = new CheckOutOrder();
-        checkOutOrder.addToOrder("grapes");
-        checkOutOrder.addToOrder("avocado");
-        checkOutOrder.addToOrder("beans");
+        checkOutOrder.addToOrder("banana");
+        checkOutOrder.addToOrder("carrot");
+        checkOutOrder.addToOrder("noodles");
         BigDecimal totalOfItems = checkOutOrder.getTotalAmount();
-        assertEquals(BigDecimal.valueOf(12.73), totalOfItems);
+        assertEquals(BigDecimal.valueOf(0.83), totalOfItems);
     }
 
     @Test
     void scannedItemsTotalTest3(){
         CheckOutOrder checkOutOrder = new CheckOutOrder();
-        checkOutOrder.addToOrder("apples");
-        checkOutOrder.addToOrder("oranges");
-        checkOutOrder.addToOrder("tomatoes");
-        checkOutOrder.removeFromOrder("apples");
-        checkOutOrder.addToOrder("avocado");
-        checkOutOrder.removeFromOrder("oranges");
-        BigDecimal totalOfItems = checkOutOrder.getTotalAmount();
-        assertEquals(BigDecimal.valueOf(3.47), totalOfItems);
+        checkOutOrder.addToOrder("noodles");
+        checkOutOrder.addToOrder("strawberry");
+        checkOutOrder.addToOrder("carrot");
+        checkOutOrder.removeFromOrder("noodles");
+        checkOutOrder.addToOrder("carrot");
+        checkOutOrder.removeFromOrder("strawberry");
+        BigDecimal totalOfItems = checkOutOrder.getTotalAmount().stripTrailingZeros();
+        assertEquals(BigDecimal.valueOf(1), totalOfItems);
     }
 }
